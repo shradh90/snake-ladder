f28ea88ee9e62f68467322fa7b3d4b308c9b6fab
shradh90 <shradhad1996@gmail.com>
2021-06-10 18:19:46 +0530

    ensureing the exact value

--- snakeandladder.java
+++ snakeandladder.java
@@ -3,15 +3,14 @@ public class snakeandladder {
 		System.out.println("**************************************************************\t");
 		System.out.println("welcome to snake and ladder game");
 		System.out.println("**************************************************************");
-		
-		int pos = 0;
+		int player=0;
 		System.out.println("at position 2,9,25,30,51,65,79,89 we have ladders");
 		System.out.println("at position 10,25,38,48 we have snakes");
 	
-	
-		 int player = (int) (Math.floor(Math.random() * 10 ) % 6) + 1;
-		
-		System.out.println(" player " + player );
+		while(player <= 100) {
+		int daice = (int) (Math.floor(Math.random() * 10 ) % 6) + 1;
+		player = daice + player; 
+		System.out.println(" player " + player ); 
 			if(player==2 || player==9 || player==25 || player==30 || player==51 || player==65 || player==79 || player==89) {
 			System.out.println("LUCKY! GOT A LADDER");
 				if(player==2){
@@ -63,11 +62,11 @@ public class snakeandladder {
                                 player=58;
                                 }
                         }
-			if(player==100){
+
+			if(player >= 100){
 			System.out.println("CONGRATULATION ! YOU WON ");
+			break;
 			}
-		
-		
+		}
 	}
 }
-
